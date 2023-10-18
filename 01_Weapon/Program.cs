7e929786e557ba61d26b8d32e061fa34dc08c621
konstantine2121 <kach89kach89@gmail.com>
2023-10-18 13:47:52 +0300

    Добавьте файлы проекта.

---
+++ 01_Weapon/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Weapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test();
+            test.Run();
+        }
+    }
+
+    class Test
+    {        
+        private const int MinHealth = 0;
+        private const int MaxHealth = 50;
+
+        private static readonly Random _random = new Random();
+
+        public void Run()
+        {
+            var players = CreatePlayers(5);
+            var bot = CreateBot(10, 50);
+
+            while (!bot.OutOfAmmo() && players.Any(player => player.IsAlive()))
+            {
+                foreach(var player in players)
+                {
+                    bot.OnSeePlayer(player);
+                }
+            }
+        }
+
+        private List<Player> CreatePlayers(uint numberOfPlayers)
+        {
+            var players = new List<Player>();
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                players.Add(new Player(GetHealth()));
+            }
+
+            return players;
+        }
+
+        private int GetHealth()
+        {
+            return _random.Next(MinHealth, MaxHealth + 1);
+        }
+
+        private Bot CreateBot(int damage, int bullets)
+        {
+            return new Bot(new Weapon(damage, bullets));
+        }
+    }
+
+    #region Extensions
+    
+    static class BotExtensions
+    {
+        public static bool OutOfAmmo(this Bot bot)
+        { 
+            return bot.Weapon.Bullets <= 0; 
+        }
+    }
+
+    static class PlayerExtensions
+    {
+        public static bool IsAlive(this Player player)
+        {
+            return player.Health > 0;
+        }
+    }
+
+    #endregion Extensions
+
+    #region Classes
+    
+    class Weapon
+    {
+        public Weapon(int damage, int bullets)
+        {
+            if (damage < 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(damage));
+            }
+            if (bullets < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bullets));
+            }
+
+            Damage = damage;
+            Bullets = bullets;
+        }
+
+        public int Damage { get; }
+
+        public int Bullets { get; private set; }
+
+        public void Fire(Player player)
+        {
+            if (Bullets <= 0)
+            {
+                return;
+            }
+
+            player.TakeDamage(Damage);
+            Bullets--;
+        }
+    }
+
+    class Player
+    {
+        public Player(int health)
+        {
+            Health = health;
+        }
+
+        public int Health { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage <= 0)
+            {
+                return;
+            }
+
+            Health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        public Bot(Weapon weapon)
+        {
+            if (weapon == null)
+            {
+                throw new ArgumentNullException(nameof(weapon));
+            }
+
+            Weapon = weapon;
+        }
+
+        internal Weapon Weapon { get; }
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
+        }
+    }
+
+    #endregion Classes
+}
---
+++ 01_Weapon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("01_Weapon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_Weapon")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3d000300-9e84-4922-9a75-15d41f1fb0b0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
