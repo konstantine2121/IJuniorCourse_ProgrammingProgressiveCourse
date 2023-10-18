5142b2f3b20f86bd3161fc2b4bb43312c7287481
konstantine2121 <kach89kach89@gmail.com>
2023-10-18 19:56:14 +0300

    Add Logger

---
+++ 03_Logger/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _03_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test();
+            test.Run();
+        }
+
+        public class Test
+        {
+            public void Run()
+            {
+                var consoleLogger = new ConsoleLogger();
+                var fileLogger = new FileLogger();
+
+                var finders = new List<Pathfinder>
+                {
+                    //1) Пишет лог в файл. 
+                    new Pathfinder(fileLogger),
+
+                    //2) Пишет лог в консоль. 
+                    new Pathfinder(consoleLogger),
+
+                    //3) Пишет лог в файл по пятницам. 
+                    new Pathfinder(new FridayLogger(fileLogger)),
+
+                    //4) Пишет лог в консоль по пятницам. 
+                    new Pathfinder(new FridayLogger(consoleLogger)),
+
+                    //5) Пишет лог в консоль а по пятницам ещё и в файл.
+                    new Pathfinder(new ConsoleLogger(new FridayLogger(fileLogger)))
+                };
+
+                finders.ForEach(finder => finder.Find());
+
+                Console.ReadKey();
+            }
+        }
+
+        public class Pathfinder
+        {
+            private const string Message = "ALARM!";
+            private readonly ILogger _logger;
+
+            public Pathfinder(ILogger logger)
+            {
+                if (logger == null) 
+                {
+                    throw new ArgumentNullException(nameof(logger));
+                }
+
+                _logger = logger;
+            }
+
+            public void Find()
+            {
+                _logger.Log(Message);
+            }
+        }
+
+        public interface ILogger
+        {
+            void Log(string message);
+        }
+
+        class ConsoleLogger: ILogger
+        {
+            private readonly ILogger _logger;
+
+            public ConsoleLogger(ILogger logger = null)
+            {
+                _logger = logger;
+            }
+
+            public void Log(string message)
+            {
+                Console.WriteLine(message);
+                _logger?.Log(message);
+            }
+        }
+
+        class FileLogger : ILogger
+        {
+            private const string LogPath = "log.txt";
+            private readonly ILogger _logger;
+
+            public FileLogger(ILogger logger = null)
+            {
+                _logger = logger;
+            }
+
+            public void Log(string message)
+            {
+                File.AppendAllText(LogPath, message + Environment.NewLine);
+                _logger?.Log(message);
+            }
+        }
+
+        class FridayLogger : ILogger
+        {
+            private readonly ILogger _logger;
+
+            public FridayLogger(ILogger logger = null)
+            {
+                _logger = logger;
+            }
+
+            public void Log(string message)
+            {
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+                {
+                    _logger?.Log(message);
+                }
+            }
+        }
+    }
+}
---
+++ 03_Logger/README.md
@@ -0,0 +1,63 @@
+﻿# Логирование
+
+## Задание
+
+Есть такая реализация логирования - https://pastebin.com/7xL6S4vV
+
+Защищённый логер даёт функционал, что логер пишется только по пятницам (такая условность).
+
+Представьте класс Pathfinder у которого есть зависимость от условного ILogger, в процессе своей работы он что-то пишет в лог. 
+Что не принципиально. 
+Сделайте в нём один метод Find который только пишет в лог через своего логера.
+
+Перепроектируйте систему логирования так, чтобы у меня было 5 объектов класса Pathfinder:
+1) Пишет лог в файл. 
+2) Пишет лог в консоль. 
+3) Пишет лог в файл по пятницам. 
+4) Пишет лог в консоль по пятницам. 
+5) Пишет лог в консоль а по пятницам ещё и в файл.
+
+## Копия исходников
+
+```cs
+using System;
+using System.IO;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class ConsoleLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+}
+```
