a15a5d843af1f49653f4733f96589cac049ff47e
Konstantine <kach91kach91@gmail.com>
2023-11-13 01:07:24 +0300

    Add 20 task

---
+++ 20_Task/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _20_Task
+{
+    //20. Группировка полей по префиксу
+    //Поправьте код - https://gist.github.com/HolyMonkey/228d407270b740387bbab0fede8fc29b
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Player
+    {
+        public Player(string name, int age, Weapon weapon, Movement movement)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name can't be empty.", nameof(name));
+            }
+
+            if (age < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(age));
+            }
+
+            Name = name;
+            Age = age;
+            Weapon = weapon ?? throw new ArgumentNullException(nameof(weapon));
+            Movement = movement ?? throw new ArgumentNullException(nameof(movement));
+        }
+
+        public string Name { get; }
+
+        public int Age { get; private set; }
+
+        public Weapon Weapon { get; private set; }
+        public Movement Movement { get; }
+    }
+
+    class Weapon
+    {
+        public Weapon(float cooldown, int damage)
+        {
+            if (cooldown < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(cooldown));
+            }
+
+            if (damage < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(damage));
+            }
+
+            Cooldown = cooldown;
+            Damage = damage;
+        }
+
+        public float Cooldown { get; private set; }
+
+        public int Damage { get; private set; }
+    }
+
+    class Movement
+    {
+        public Movement(float speed, Direction direction)
+        {
+            if (speed < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(speed));
+            }
+
+            Speed = speed;
+            Direction = direction ?? throw new ArgumentNullException(nameof(direction)); ;
+        }
+
+        public float Speed { get; private set; }
+
+        public Direction Direction { get; private set; }
+        
+    }
+
+    class Direction
+    {
+        public Direction(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float X { get; private set; }
+
+        public float Y { get; private set; }
+    }
+
+
+}
---
+++ 20_Task/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("20_Task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20_Task")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("247c0914-754a-4d4b-899b-c8a49b1b1a4e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
